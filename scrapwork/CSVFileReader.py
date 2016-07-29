a49e9d7ea0347b8953a4218048f9082a19b8a808
Daniel Streb <danielrstreb@gmail.com>
2016-07-29 12:25:51 -0700

    Added documentation concerning csv.reader validation and creation

--- scrapwork/CSVFileReader.py
+++ scrapwork/CSVFileReader.py
@@ -2,10 +2,15 @@
 
 import csv
 
+# Take file name as raw string
 testData = r'DummyData(no_header).csv'
 
 with open(testData, 'rb') as csvfile:
+    # Determines the dialect of the csv file for processing
     dialect = csv.Sniffer().sniff(csvfile.read(1024))
-    csvfile.seek(0)  # Resets the read/write pointer within the file
+    # Resets the read/write pointer within the file
+    csvfile.seek(0)
+    # Creates a reader object with the csvfile provided, and the dialect
+    # object to define the parameters of the reader instance.
     reader = csv.reader(csvfile, dialect)
     print("Header: {}".format(csv.Sniffer().has_header(csvfile.read(1024))))
