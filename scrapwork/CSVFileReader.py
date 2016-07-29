e245a949106b0dd400c1cd09ae0264d6efca92a4
Daniel Streb <danielrstreb@gmail.com>
2016-07-29 13:31:21 -0700

    Made code more readable with whitespace. Created means of filtering data from csvfile dictionary

--- scrapwork/CSVFileReader.py
+++ scrapwork/CSVFileReader.py
@@ -5,20 +5,28 @@ import csv
 # Take file name as raw string
 testData = r'DummyData.csv'
 
+# Opening testData binary file for reading, hence 'rb', as 'csvfile'.
 with open(testData, 'rb') as csvfile:
     # Determines the dialect of the csv file for processing
     file_dialect = csv.Sniffer().sniff(csvfile.read(1024))
+
     # Resets the read/write pointer within the file
     csvfile.seek(0)
+
     # Creates a reader object with the csvfile provided, and the dialect
     # object to define the parameters of the reader instance.
     reader = csv.DictReader(csvfile, dialect=file_dialect)
+
     # Checks to see that the csv file imported has a header row,
     # that will be used for later parsing.
     print("Header: {}".format(csv.Sniffer().has_header(csvfile.read(1024))))
     print('Delimiter: "{}"'.format(file_dialect.delimiter))
+
     # Resets the read/write pointer within the file
     csvfile.seek(0)
 
+    # Turn this block into a function for filtering columns of data
+    column = 'Time'
     for row in reader:
-        print(row['Time'])
+        print(row.get(column))  # Returns all values from column.
+        # print(row['Time'])
