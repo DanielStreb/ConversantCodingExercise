94454bc79d20a1de6290ae972ce551da11fd0d12
Daniel Streb <danielrstreb@gmail.com>
2016-07-28 18:35:30 -0700

    Fixed

--- DataParser.py
+++ DataParser.py
@@ -2,8 +2,15 @@
 
 
 class DataParser:
+<<<<<<< HEAD
     """Class description."""
 
     def __init__(self):
         """Method description."""
+=======
+    """Class."""
+
+    def __init__(self):
+        """Method."""
+>>>>>>> ba8d5d0ad97f154bf7f8e9e16f8e8ee24d971df9
         pass
