abec7f487945af2ab78e287776d157ccbac55bf0
Daniel Streb <danielrstreb@gmail.com>
2016-07-29 16:41:51 -0700

    Created program file for testing csv file reader functions

---
+++ scrapwork/Test.py
@@ -0,0 +1,37 @@
+"""CSV File Reader."""
+
+from csv import Sniffer, DictReader
+
+# Take file name as raw string
+# test_file = r'DummyData.csv'
+test_file = r'DummyData(negative numbers+decimal numbers+letters).csv'
+
+
+def valid_number(s):
+    """
+    Summary: Checks that value is a valid positive number.
+
+    Description: Accepts positive whole and decimal numbers.
+    """
+    try:
+        float(s)
+        if int(s) > 0:
+            return True
+        return False
+    except ValueError:
+        return False
+
+with open(test_file, 'rb') as csvfile:
+    csvfile.seek(0)
+    file_dialect = Sniffer().sniff(csvfile.read(1024))
+    csvfile.seek(0)
+    reader = DictReader(csvfile, dialect=file_dialect)
+    csvfile.seek(0)
+
+    column = 'A'
+    for row in reader:
+        value = row.get(column)
+        if not valid_number(value):
+            print("Bad Value: {}".format(value))
+        else:
+            print("Good Value: {}".format(value))
