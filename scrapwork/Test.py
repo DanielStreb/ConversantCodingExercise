43246591c47503f8d71455bd63c89a7bce527300
Daniel Streb <danielrstreb@gmail.com>
2016-07-29 17:13:41 -0700

    Updated to grab filtered records from dataset

--- scrapwork/Test.py
+++ scrapwork/Test.py
@@ -4,7 +4,8 @@ from csv import Sniffer, DictReader
 
 # Take file name as raw string
 # test_file = r'DummyData.csv'
-test_file = r'DummyData(negative numbers+decimal numbers+letters).csv'
+# test_file = r'DummyData(negative numbers+decimal numbers+letters).csv'
+test_file = r'Data(Relevant).csv'
 
 
 def valid_number(s):
@@ -14,24 +15,40 @@ def valid_number(s):
     Description: Accepts positive whole and decimal numbers.
     """
     try:
+        # Checking that entered value can be converted to a float.
+        # Excludes letters and symbols.
         float(s)
+
+        # Checking that validated number is nonnegative.
         if float(s) > 0:
             return True
         return False
     except ValueError:
         return False
 
+# Opening data binary file for reading, hence 'rb', as 'csvfile'.
 with open(test_file, 'rb') as csvfile:
-    csvfile.seek(0)
-    file_dialect = Sniffer().sniff(csvfile.read(1024))
-    csvfile.seek(0)
-    reader = DictReader(csvfile, dialect=file_dialect)
+    # Resetting read/write pointer to beginning of file
     csvfile.seek(0)
 
-    column = 'C'
+    file_dialect = Sniffer().sniff(csvfile.read(1024))
+
+    # Resetting read/write pointer to beginning of file
+    csvfile.seek(0)
+
+    reader = DictReader(csvfile, dialect=file_dialect)
+
+    # Resetting read/write pointer to beginning of file
+    csvfile.seek(0)
+
+    goodRecords = []
+    ignoredRecords = []
+
+    dataCenters = ['I', 'A', 'S']
     for row in reader:
-        value = row.get(column)
-        if not valid_number(value):
-            print("Bad Value: {}".format(value))
-        else:
-            print("Good Value: {}".format(value))
+        if row.get('DC') in dataCenters:
+            value = row.get('Value')
+            if not valid_number(value):
+                ignoredRecords.append(row)
+            else:
+                goodRecords.append(row)
