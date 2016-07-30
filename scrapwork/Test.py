f00064f74477d4076737ede8104a0dd683980d95
Daniel Streb <danielrstreb@gmail.com>
2016-07-29 17:29:06 -0700

    Added create_reader function to test file

--- scrapwork/Test.py
+++ scrapwork/Test.py
@@ -6,6 +6,35 @@ from csv import Sniffer, DictReader
 # test_file = r'DummyData.csv'
 # test_file = r'DummyData(negative numbers+decimal numbers+letters).csv'
 test_file = r'Data(Relevant).csv'
+dataCenters = ['I', 'A', 'S']
+
+
+def create_reader(csvfile=None):
+    """
+    Summary: Validates a csv file, returns a DictReader object.
+
+    Description: Takes one argument: "data" (Should be a csv file)
+    """
+    # Determines the dialect of the csv file for processing
+    file_dialect = Sniffer().sniff(csvfile.read(1024))
+
+    # Resets the read/write pointer within the file
+    csvfile.seek(0)
+
+    # Checks to see that the csv file imported has a header row,
+    # that will be used for later parsing.
+    print("Header: {}".format(Sniffer().has_header(csvfile.read(1024))))
+    print('Delimiter: "{}"'.format(file_dialect.delimiter))
+
+    # Resets the read/write pointer within the file
+    csvfile.seek(0)
+
+    # Creates a DictReader object with the csvfile provided, and the
+    # dialect object to define the parameters of the reader instance.
+    reader = DictReader(csvfile, dialect=file_dialect)
+
+    # Return DictReader object
+    return reader
 
 
 def valid_number(s):
@@ -31,12 +60,8 @@ with open(test_file, 'rb') as csvfile:
     # Resetting read/write pointer to beginning of file
     csvfile.seek(0)
 
-    file_dialect = Sniffer().sniff(csvfile.read(1024))
-
-    # Resetting read/write pointer to beginning of file
-    csvfile.seek(0)
-
-    reader = DictReader(csvfile, dialect=file_dialect)
+    # reader = DictReader(csvfile, dialect=file_dialect)
+    reader = create_reader(csvfile)
 
     # Resetting read/write pointer to beginning of file
     csvfile.seek(0)
@@ -44,7 +69,6 @@ with open(test_file, 'rb') as csvfile:
     goodRecords = []
     ignoredRecords = []
 
-    dataCenters = ['I', 'A', 'S']
     for row in reader:
         if row.get('DC') in dataCenters:
             value = row.get('Value')
