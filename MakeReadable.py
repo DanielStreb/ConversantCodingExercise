30aa1f245d5807674f6a4603dc018867ecd5da6b
Daniel Streb <danielrstreb@gmail.com>
2016-07-30 00:48:30 -0700

    Modified minor things

--- MakeReadable.py
+++ MakeReadable.py
@@ -1,10 +1,14 @@
 """MakeReadable.py."""
-import time
+from datetime import datetime
+import matplotlib.dates as dt
+
+from pytz import utc
 
 
 def human_readable(epoch_time=0):
     """Function to convert epoch time to a human-readable format."""
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_time))
+    return dt.date2num(datetime.fromtimestamp(epoch_time, utc))
 
 print(human_readable(1347517370))
-print(human_readable())
+# print(human_readable())
+# print(time.asctime(time.gmtime(1347517370)))
