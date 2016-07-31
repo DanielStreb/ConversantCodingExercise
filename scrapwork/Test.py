07b965585a425d31c4851b8f02f14a295f0077f5
Daniel Streb <danielrstreb@gmail.com>
2016-07-30 19:07:46 -0700

    Added "format_axes" helper function for graphing function use

--- scrapwork/Test.py
+++ scrapwork/Test.py
@@ -14,6 +14,37 @@ test_file = r'Data(Relevant).csv'
 dataCenters = ('I', 'A', 'S')
 
 
+def format_axes(ax=None):
+    """
+    Summary: helper function for customizing graph display.
+
+    Create and set major/minor locators for graph's major/minor ticks.
+    Format dates/times of x axis.
+    Autoscale graph view.
+    Show grid on graph.
+    """
+    # HourLocator creates major ticks every 2 hours in a 24 hour period
+    hours = HourLocator(byhour=range(24), interval=2)
+    ax.xaxis.set_major_locator(hours)
+
+    # MinuteLocator creates minor ticks every 30 minutes in a 1 hour period
+    minutes = MinuteLocator(byminute=range(60), interval=30)
+    ax.xaxis.set_minor_locator(minutes)
+
+    # Creates format 'Hour:MinutePeriod Month/Day/Year' x-axis ticks
+    # Example: '5:30PM 9/23/15'
+    time_fmt = DateFormatter('%H:%M%p %x')
+    ax.xaxis.set_major_formatter(time_fmt)
+
+    # Formats x-axis information 'Weekday Month Day Hour:MinutePeriod'
+    # Example: 'Tuesday Sep 23 12:15PM'
+    ax.fmt_xdata = DateFormatter('%A %b %d %H:%M%p')
+
+    ax.autoscale_view()
+
+    ax.grid(True)
+
+
 def create_reader(csvfile=None):
     """
     Summary: Validates a csv file, returns a DictReader object.
@@ -124,10 +155,8 @@ def graph_dataset(data_center_list=None, dataset_to_graph=None):
     # Creating figure and axes objects for display
     fig, ax = plt.subplots()
 
-    # HourLocator creates major ticks every 2 hours in a 24 hour period
-    hours = HourLocator(byhour=range(24), interval=2)
-    # MinuteLocator creates minor ticks every 30 minutes in a 1 hour period
-    minutes = MinuteLocator(byminute=range(60), interval=30)
+    # Formatting y and x axis for effective plotting
+    format_axes(ax)
 
     # Iterating through list of data centers to be plotted together.
     for dc in data_center_list:
@@ -136,21 +165,6 @@ def graph_dataset(data_center_list=None, dataset_to_graph=None):
 
         # Add data center to list of plotted dcs for dynamic legend
         plotted_dc.append(dc)
-
-    # Creates format 'Hour:MinutePeriod Month/Day/Year' x-axis ticks
-    # Example: '5:30PM 9/23/15'
-    time_fmt = DateFormatter('%H:%M%p %x')
-
-    # Formats x-axis information 'Weekday Month Day Hour:MinutePeriod'
-    # Example: 'Tuesday Sep 23 12:15PM'
-    ax.fmt_xdata = DateFormatter('%A %b %d %H:%M%p')
-
-    ax.xaxis.set_major_locator(hours)
-    ax.xaxis.set_major_formatter(time_fmt)
-    ax.xaxis.set_minor_locator(minutes)
-    ax.autoscale_view()
-
-    ax.grid(True)
 
     fig.autofmt_xdate()
 
