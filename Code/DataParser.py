40640e3999d1426d561e5fea4ed51ae06d86954d
Daniel Streb <danielrstreb@gmail.com>
2016-07-30 19:56:11 -0700

    Created new directories for code and docs

---
+++ Code/DataParser.py
@@ -0,0 +1,15 @@
+"""DataParser."""
+
+# import csv
+
+
+class DataParser:
+    """Class description."""
+
+    def __init__(self):
+        """Method Definition."""
+        self.csvfile = None
+
+    def create_reader(self, csvfile):
+        """Method Definition."""
+        pass
