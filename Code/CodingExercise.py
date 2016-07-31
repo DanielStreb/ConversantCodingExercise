aa7a732ac7669f58e94ebe5e42334057b3cb53f7
Daniel Streb <danielrstreb@gmail.com>
2016-07-30 23:11:21 -0700

    Defined main function in CodingExercise.py

--- Code/CodingExercise.py
+++ Code/CodingExercise.py
@@ -3,13 +3,20 @@
 from DataParser import DataParser
 from Grapher import Grapher
 
-# Take file name as raw string
-test_file = r'Data(Relevant).csv'
 
-# List of valid data centers
-dataCenters = ('A', 'S')
+def main():
+    """Main Function."""
+    # Take file name as raw string
+    test_file = r'Data(Relevant).csv'
 
-dp = DataParser(test_file, dataCenters)
-dataset = dp.get_dataset()
-gr = Grapher(dataset, dataCenters)
-gr.graph_dataset()
+    # List of valid data centers
+    data_centers = ('I', 'A', 'S')
+
+    dp = DataParser(test_file, data_centers)
+    dataset = dp.get_dataset()
+    gr = Grapher(dataset, data_centers)
+    gr.graph_dataset()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
