e19299109b51cf59b0698d1d61f09a75e20e0aa1
Daniel Streb <danielrstreb@gmail.com>
2016-07-30 19:50:10 -0700

    Started class design in 'Test.py'

--- DataParser.py
+++ DataParser.py
@@ -9,5 +9,7 @@ class DataParser:
     def __init__(self):
         """Method Definition."""
         self.csvfile = None
+
     def create_reader(self, csvfile):
+        """Method Definition."""
         pass
