0d2c06be52e44ae3c9e2761f18f276d72a2a59fb
Daniel Streb <danielrstreb@gmail.com>
2016-07-31 17:22:23 -0700

    Updated

--- Code/CodingExercise.py
+++ Code/CodingExercise.py
@@ -11,21 +11,26 @@ def main():
 
     # Regular expression that only accepts strings with valid DC
     # names 'I', 'A', and 'S' separated by spaces
-    my_regex = r'[^A|^I|^S][ A| S| I]?[ A$| I$| S$]'
+    my_regex = r'^[IAS]$'
+
+    dcs = []
+
+    print('Enter data centers to be graphed')
 
     while True:
-        dc_string = input("Data Centers to graph: ")
-        if re.search(my_regex, dc_string + ' '):
+        dc_string = input("\t> ")
+        if re.search(my_regex, dc_string): 
+            dcs.append(dc_string)
+        elif dc_string == '':
             break
         else:
             print("Valid DCs: 'I' 'A' 'S'")
 
-    data_centers = set(dc_string.split())
-    print(data_centers)
+    dcs = set(dcs)
 
-    dp = DataParser(test_file, data_centers)
+    dp = DataParser(test_file, dcs)
     dataset = dp.get_dataset()
-    gr = Grapher(dataset, data_centers)
+    gr = Grapher(dataset, dcs)
     gr.graph_dataset()
 
 if __name__ == "__main__":
